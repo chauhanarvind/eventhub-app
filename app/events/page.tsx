cca35dd9117a89179249376977b6438f6b0d7391
chauhanarvind <chauhan.m.arvind@gmail.com>
2024-09-26 10:15:53 +1200

    saving

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -15,7 +15,7 @@ const EventPage = () => {
     async function fetchEvents() {
       try {
         const response = await fetch(
-          "https://ec2-34-229-185-121.compute-1.amazonaws.com/api/Items",
+          "https://ec2-34-229-185-121.compute-1.amazonaws.com/api/items",
           {
             method: "GET",
           }
